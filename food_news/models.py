18605b90c0478682faa87bc05059220afb19de1e
unknown <sekerbekovbeksultan2@gmail.com>
2023-11-22 12:15:39 +0600

    change models

--- food_news/models.py
+++ food_news/models.py
@@ -1,3 +1,10 @@
+# cafe_news/models.py
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
