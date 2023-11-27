ae17a66666fbe441d75ee2bffe96068553ce3b2d
Lay7415 <ieye7531@gmail.com>
2023-11-27 22:17:22 +0600

    add css to main.html

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from food.models import Food
 
 # Create your views here.
 def main_page_view(request):
-    return render(request, 'main/main.html')
+    foods = Food.objects.all()
+    return render(request, 'main/main.html', {"foods": foods})
