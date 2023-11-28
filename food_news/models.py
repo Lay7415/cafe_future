59bdcd08048f73044f2020d0757d3189beb8ff2c
unknown <sekerbekovbeksultan2@gmail.com>
2023-11-28 08:26:12 +0600

    last commit

--- food_news/models.py
+++ food_news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
     image = models.ImageField(upload_to='', null=True)
 
     def __str__(self):
